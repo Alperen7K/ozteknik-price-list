f044f56095cebfd7d562f0e54ea7b7e466311eb4
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2023-11-21 21:15:42 +0300

    Geçici buton eklendi

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,15 @@
  * * Written by Mehmet Alperen Yedik <mehmetalperenyedik@gmail.com>, October 2023
  */
 import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 export const Home = () => {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/price/1");
+  }, []);
   return <div>Home</div>;
 };
